6371311781ecef8b730548c3abb50dcbe8c9dd37
Pascal Martineau <pascal@lewebsimple.ca>
2025-12-14 08:54:10 -0500

    minor: disable headers forwarding for now

--- src/runtime/composables/useGraphQL.ts
+++ src/runtime/composables/useGraphQL.ts
@@ -1,16 +1,8 @@
 import type { RequestDocument, Variables } from "graphql-request";
-import { useNuxtApp, useRequestHeaders } from "#imports";
+import { useNuxtApp } from "#imports";
 
 export function useGraphQL() {
   const { $graphql } = useNuxtApp();
-
-  // Forward cookies on server for auth
-  if (import.meta.server) {
-    const headers = useRequestHeaders(["cookie"]);
-    if (headers.cookie) {
-      $graphql.setHeader("cookie", headers.cookie);
-    }
-  }
 
   return {
     request: <T>(document: RequestDocument, variables?: Variables) => $graphql.request<T>(document, variables),
